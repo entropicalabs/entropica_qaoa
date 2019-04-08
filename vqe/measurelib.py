8c64153be793bb3222fa44e7e23c56e374543410
janlukas.bosse <janlukas.bosse@gmail.com>
2019-04-08 18:28:46 +0200

    beautify

--- vqe/measurelib.py
+++ vqe/measurelib.py
@@ -64,39 +64,3 @@ def hamiltonian_expectation_value(hamiltonian, bitstrings):
 
     return (np.mean(energies),
             np.sqrt(np.var(energies)) / np.sqrt(bitstrings.shape[0]))
-
-#
-# bitstrings = np.array([[1,0], [0,1], [1,1], [1,1]])
-#
-# ham = PauliSum.from_compact_str("1.0*Z0*Z1 + 0.5*Z0 + (-1)*Z1")
-# hamiltonian_expectation_value(ham, bitstrings)
-#
-# import sys, os
-# myPath = os.path.dirname(os.path.abspath(__file__))
-# sys.path.insert(0, myPath + '/../')
-#
-# from vqe.cost_function import prep_and_measure_ham_qvm
-# from qvm_process import qvm_process
-#
-# from pyquil.paulis import PauliSum, PauliTerm
-# from pyquil.gates import RX, RY, H, CNOT
-# from pyquil.quil import Program, QubitPlaceholder, address_qubits
-#
-# import numpy as np
-#
-#
-# proc = qvm_process("9q-qvm")
-# qvm = proc.qvm
-# sim = proc.sim
-#
-# q0 = QubitPlaceholder()
-# q1 = QubitPlaceholder()
-# p = Program(H(q0), RX(np.pi/2, q1))
-# p = append_measure_register(p, trials=100)
-# print(p)
-# qubit_mapping = {q0: 0, q1: 1}
-# p = address_qubits(p, qubit_mapping=qubit_mapping)
-# print(p)
-# exe = qvm.compile(p)
-# bitstrings = qvm.run(exe)
-# print(bitstrings)
