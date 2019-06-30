cb78b8860170229f72c846e2c8bf14a4826b9e65
Ewan Munro <ewan@entropicalabs.com>
2019-07-01 07:19:36 +0800

    modified landscapes files

--- landscapes.py
+++ landscapes.py
@@ -1,11 +1,20 @@
 import numpy as np
-import scipy.signal as signal
 import matplotlib.pyplot as plt
 from matplotlib import cm
 from mpl_toolkits.mplot3d import Axes3D 
-
+from itertools import product
 from typing import Iterable, Union, List, Tuple, Any, Type
 
+"""
+TODO:
+    
+    - At the moment all methods are hard-coded to plot the cost function, but should be generalised to allow 
+    visualistion of the standard deviation too. Also, they might be combined onto a single plot
+    
+    - Be wary of there being too many attributes.
+    
+    - Methods that operate on the output of get_local_minima
+"""
 
 class landscape:
     
@@ -17,47 +26,21 @@ class landscape:
         self.the_parameters = the_params
         self.parameter_ranges = param_ranges
         self.func_vals, self.dev_vals = func_vals
-        self.shape = np.shape(func_vals)
+        self.shape = np.shape(func_vals[0])
         self.local_mins = []
-        
+        self.larger_neighbours = []
         
         # Have a nice str or repr method for the class attributes
-
-    def local_minima(self): # Static or class method?
         
-        """
-        Finds the list of local minima in the desired array
-        signal.argrelmax finds maxima, so we find the maxima of -1*array
-        signal.argrelmax returns any kind of minima (eg along any of the directions), not necessarily a minima in all directions
-        we thus further analyse the result to obtain those points that are true local minima in all directions
-        note that this method ignores points at the extremes of a given dimension
-        
-        TODO: systematically ensure this method is correct by constructing landscapes with known local minima & make sure it returns right result
-        """
-         
-        mins = signal.argrelmax(-1*self.func_vals)
-        mins = [[mins[0][i],mins[1][i]] for i in range(len(mins[0]))]
-        
-        local_mins = []
-        for i in mins: 
-            
-            val_here = self.func_vals[tuple(i)] # Function value at coordinate i
-            
-            # Get indices of neighbouring points, checking if the point on the boundary of the coordinate system (ignore if so)
-            edge_pts = [j for j in range(len(i)) if i[j] == self.shape[j]-1 or i[j] == 0]
-            if edge_pts: 
-                continue
-            else:
-                nbours = np.array([i]*4) + np.array([[-1,0], [0,-1], [1, 0], [0,1]]) # coordinates of neighbouring points (NEED TO EXTEND TO ARBITRARY DIMS)
-            
-            val_nbours = [self.func_vals[tuple(j)] for j in nbours] # Function values at neighbouring points
-
-            if all(val_nbours > val_here): # "Greater than" because argrelmax finds peaks
-                local_mins.append(i)
-                
-        self.local_mins = local_mins
-            
+    ########### PLOTTING METHODS ##################
+       
     def plot_contour(self): # Static or class method?
+        
+        """
+        Contour plot of the landscape
+        Needs to be cleaned up and better presented
+        Figure out why this is useful
+        """
         
         assert (len(self.the_parameters) == 2), "Two parameters required for visualisation on 3D plot."
         
@@ -86,10 +69,65 @@ class landscape:
         
         fig.colorbar(surf, shrink=0.5, aspect=5)
         plt.show()
+        
+    def plot_image(self):
 
+        """
+        Plots an image of the cost function values
+        TODO: label the plot axes correctly
+        """
+        plt.imshow(self.func_vals)
+        plt.colorbar()
+        plt.show()
 #        
 #    def plot_slice():
 #        
-#        
+    ########### LANDSCAPE ANALYSIS METHODS ##################
+        
+    def get_local_minima(self): # Static or class method?
+    
+        """
+        Searches through an array to find locations of minima
+        Since there may be multiple minima along any direction, a log(N) binary search cannot be used.
+        This method exhaustively walks through element by element, comparing to neighbours
+        """
+        dims = self.shape
+        inds_ranges = dims - np.ones(len(dims),)
+        n_els = np.prod(dims)    
+        
+        iterables = [[-1,0,1]]*len(dims)
+        neighb_array = [np.array(t) for t in product(*iterables)]
+        n_neighbs = len(neighb_array)
+        
+        neighs_above = np.zeros(dims)
+        for i in range(n_els):
+    
+            inds = np.unravel_index(i, tuple(dims))
+            val_here = self.func_vals[tuple(inds)]
+            
+            neighb_vals = np.zeros((n_neighbs,))
+            for j in range(n_neighbs):
+                
+                neighb_inds = inds + neighb_array[j]
+                
+                above_range = np.nonzero(neighb_inds > inds_ranges)
+                below_range = np.nonzero(neighb_inds < 0)
+                out_of_range = np.hstack((above_range,below_range))
+                
+                if len(out_of_range[0]) > 0 :
+                    continue
+                
+                neighb_vals[j] = self.func_vals[tuple(neighb_inds)]
+            
+            larger_neighbs = sum(neighb_vals > val_here) 
+    
+            if larger_neighbs == n_neighbs-1:
+                self.local_mins.append(inds) # Definite minimum
+            
+            neighs_above[tuple(inds)] = larger_neighbs
+    
+        self.larger_neighbours = neighs_above
+
+        
 #class landscape_compare:
 
