d14b246cd4b66f92e8a818d57a51a462e5cb7016
Ewan Munro <ewan@entropicalabs.com>
2019-07-17 17:47:26 +0800

    updated AdvancedParameter notebook

--- setup.py
+++ setup.py
@@ -1,3 +1,17 @@
+#   Copyright [yyyy] [name of copyright owner]
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
 try:
     from setuptools import setup, find_packages
 except ImportError:
@@ -5,9 +19,9 @@ except ImportError:
 
 
 setup(
-    name="forest_qaoa",
-    version="0.1dev",
-    description="QAOA implementation on top of pyQuil",
+    name="entropica_qaoa",
+    version="0.1",
+    description="Entropica Labs QAOA package",
     author="Entropica Labs: Jan Lukas Bosse, Ewan Munro",
     packages=find_packages(),
     install_requires=['numpy >= 1.7', 'scipy >= 0.9',
