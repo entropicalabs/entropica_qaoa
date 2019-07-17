68153f1b455a29b69ca9d349fdb0eac3e334ea13
Ewan Munro <ewan@entropicalabs.com>
2019-07-17 18:29:01 +0800

    added license headers to .py files

--- entropica_qaoa/qaoa/parameters.py
+++ entropica_qaoa/qaoa/parameters.py
@@ -1,4 +1,19 @@
-"""Different parametrizations of QAOA circuits.
+# Copyright 2019 Entropica Labs
+#
+#   Licensed under the Apache License, Version 2.0 (the "License");
+#   you may not use this file except in compliance with the License.
+#   You may obtain a copy of the License at
+#
+#       http://www.apache.org/licenses/LICENSE-2.0
+#
+#   Unless required by applicable law or agreed to in writing, software
+#   distributed under the License is distributed on an "AS IS" BASIS,
+#   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+#   See the License for the specific language governing permissions and
+#   limitations under the License.
+
+"""
+Different parametrizations of QAOA circuits.
 
 This module holds an abstract class to store QAOA parameters in and (so far)
 five derived classes that allow for more or less degrees of freedom in the QAOA
